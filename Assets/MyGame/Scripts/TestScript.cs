0e709dc5ea3eee9add0729f06a2c62919f78065b
Qun An Lin <qunan.lin04@htl-salzburg.ac.at>
2023-09-27 09:25:09 +0200

    Partner Aenderung Commit

--- Assets/MyGame/Scripts/TestScript.cs
+++ Assets/MyGame/Scripts/TestScript.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEngine;
 
 public class TestScript : MonoBehaviour
@@ -6,7 +5,7 @@ public class TestScript : MonoBehaviour
     void Start()
     {
         Debug.Log("Hello World");
+        Debug.Log("Hallo Lily");
     }
-
  
 }
